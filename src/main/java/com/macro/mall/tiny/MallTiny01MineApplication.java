428e859803ebb4c6008e80ceafd38931f8708ffe
unknown <734507707@qq.com>
2020-07-21 21:29:04 +0800

    Initial commit

---
+++ src/main/java/com/macro/mall/tiny/MallTiny01MineApplication.java
@@ -0,0 +1,13 @@
+package com.macro.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallTiny01MineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallTiny01MineApplication.class, args);
+	}
+
+}
